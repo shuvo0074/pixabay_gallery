38dc482a3651aa1cfc2e69ec2896b63aa218e470
Tarmim Hoque Shuvo <tarmimhoqueshuvo@Tarmims-MacBook-Pro.local>
2020-07-10 23:36:42 +0600

    remove unused import

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
+console.disableYellowBox = true
 
 import {AppRegistry} from 'react-native';
-import App from './src/routes';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => require('./src/routes').default);
